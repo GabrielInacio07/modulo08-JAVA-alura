10a93cb665560ec1fdf435f05adf5849ed63e26e
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-02-09 01:11:52 -0300

    Sexto Desafio pegar a data de um evento e comparar com a data atual, e dizer se o eventou já passou, está ocorrendo ou acontecerá

--- data-hora/src/Main.java
+++ data-hora/src/Main.java
@@ -4,18 +4,26 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
-
+//DateTimeFormatter templateDateBR = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 class Main {
     public static void main(String[] args) {
-        System.out.println("Quarto Desafio");
+        System.out.println("Sexto Desafio");
 
-        LocalDate dataInicio = LocalDate.of(2026,3,1);
-        LocalDate prazoEntrega = dataInicio.plusDays(60);
+        LocalDate dataEvento = LocalDate.of(2026,3,1);
+        LocalDate dataAtual = LocalDate.now();
 
         DateTimeFormatter templateDateBR = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-        System.out.println("Data de inicio do Projeto: " + dataInicio.format(templateDateBR));
-        System.out.println("Prazo de entrega: " + prazoEntrega.format(templateDateBR));
+        System.out.println("Data do evento: " + dataEvento.format(templateDateBR));
+        System.out.println("Data atual: " + dataAtual.format(templateDateBR));
+
+        if (dataEvento.isBefore(dataAtual)) {
+            System.out.println("O evento já ocorreu");
+        } else if (dataEvento.isAfter(dataAtual)) {
+            System.out.println("O evento ainda está por vir");
+        } else {
+            System.out.println("O evento está acontecendo hoje");
+        }
 
 
     }
