a9b1b10f7139d98cce4e554e544c5cd30476d1ce
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-02-09 01:47:50 -0300

    Oitavo Desafio pegar a hora atual BR e converter para o HÓRARIO de TOQUIO

--- data-hora/src/Main.java
+++ data-hora/src/Main.java
@@ -1,26 +1,20 @@
 import java.text.DateFormat;
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.Period;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 //DateTimeFormatter templateDateBR = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 class Main {
     public static void main(String[] args) {
-        System.out.println("Sétimo Desafio");
+        System.out.println("Oitavo Desafio");
 
-        LocalDate dataVencimento = LocalDate.of(2025, 3, 30);
-        Period antecedencia = Period.ofDays(20);
+        ZonedDateTime dataAtualBR = ZonedDateTime.now();
+        ZonedDateTime dataAtualToquio =
+                dataAtualBR.withZoneSameInstant(ZoneId.of("Asia/Tokyo"));
 
-        LocalDate dataLembrete = dataVencimento.minus(antecedencia);
+        DateTimeFormatter formatterHora =
+                DateTimeFormatter.ofPattern("HH:mm:ss");
 
-        DateTimeFormatter templateDateBR = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-        System.out.println("Data de vencimento: " + dataVencimento.format(templateDateBR));
-        System.out.println("Antecedência: " + antecedencia.getDays() + " dias");
-        System.out.println("Data do lembrete: " + dataLembrete.format(templateDateBR));
-
+        System.out.println("Horário atual em Tóquio: " + dataAtualToquio.format(formatterHora));
 
     }
 }
